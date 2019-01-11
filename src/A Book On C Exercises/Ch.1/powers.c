519dda114d3fea94a4db972297bd4a99926ca918
Aaron Earl <com_tech12@yahoo.com>
2019-01-11 15:21:03 -0800

    added comments to Ex 12

--- src/A Book On C Exercises/Ch.1/powers.c
+++ src/A Book On C Exercises/Ch.1/powers.c
@@ -8,16 +8,20 @@
 */
 
 #include <stdio.h>
-#include <math.h>
+#include <math.h> //To use pow
 
 int main(void)
 {
     int i; //Counter Storage
 
+    //Print Header, 12 to align with table header
     printf("%12s   A TABLE OF POWERS   :::::::\n", ":::::::");
+    //Print header, allow enough space for integers
     printf("%12s%12s%12s%12s%12s\n", "Integer", "Square", "3rd Power", "4th Power", "5th Power");
+    //Print header base bar
     printf("%12s%12s%12s%12s%12s\n", "-------", "------", "---------", "---------", "---------");
     
+    //Count up and perform arithmetic
     for(i = 1; i <= 100; ++i)
     {
         printf("%12d%12.0f%12.0f%12.0f%12.0f\n", i, pow(i, 2), pow(i, 3), pow(i, 4), pow(i, 5));
